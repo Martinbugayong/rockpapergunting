d98aaa3f9393ce01206585c9e07e471aa07ae868
martinbugayong <martinbugayong@gmail.com>
2019-07-22 23:11:02 -0700

    add game logic

--- scriptRPS.js
+++ scriptRPS.js
@@ -5,12 +5,18 @@ const scissorsButton = document.getElementById('scissorsButton')
 const lizardButton = document.getElementById('lizardButton')
 const spockButton = document.getElementById('spockButton')
 
+// grab inner HTML of elements in the document
+const rock = rockButton.innerHTML;
+const paper = paperButton.innerHTML;
+const scissors = scissorsButton.innerHTML;
+const lizard = lizardButton.innerHTML;
+const spock = spockButton.innerHTML;
+
 // initialize default game state
 let playerScore = 0;
 let computerScore = 0;
 let roundNumber = 1;
 let playerChoice, computerChoice; // both undefined here
-console.log(playerChoice) // output 'undefined'
 
 // event listiners for all buttons on HTML
 rockButton.addEventListener('click', isClicked)
@@ -20,24 +26,57 @@ lizardButton.addEventListener('click', isClicked)
 spockButton.addEventListener('click', isClicked)
 
 // click function
-function isClicked (e){
+function isClicked(e) {
     getPlayerChoice(e);
+    getComputerChoice(e);
+    getPlayerScore();
 }
 
-// create player game logic  
-function getPlayerScore (){
-
+// create playerChoice function that gets player's choice
+function getPlayerChoice(e) {
+    playerChoice = e.target.innerHTML;
 }
 
-// create function that gets player's choice
-function getPlayerChoice(e){
-    playerChoice = e.target.id; 
-    console.log(playerChoice) 
+// create computerChoice function
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 5)
+    switch (randomNumber) {
+        case 0:
+            computerChoice = rock;
+            break;
+        case 1:
+            computerChoice = paper;
+            break;
+        case 2:
+            computerChoice = scissors;
+            break;
+        case 3:
+            computerChoice = lizard;
+            break;
+        case 4:
+            computerChoice = spock;
+    }
 }
 
-// create computer game logic
+// create win condition.  
+function getPlayerScore() {
 
-// create score 
+    if (playerChoice === rock && computerChoice === scissors || lizard, 
+        playerChoice === paper && computerChoice === rock || spock, 
+        playerChoice === scissors && computerChoice === paper || lizard, 
+        playerChoice === lizard && computerChoice === spock || paper, 
+        playerChoice === spock && computerChoice === scissors || rock
+    ) {
+        console.log(playerChoice + " vs " + computerChoice)
+    } else if (
+        playerChoice === computerChoice 
+    ) {
+        console.log('its a draw')
+    }
+}
+
+// create computer win condition 
+
 
 // create round 
 
