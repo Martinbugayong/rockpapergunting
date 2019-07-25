537c90ca2a1deca622bb3b17c6667f03d43fd8c8
martinbugayong <martinbugayong@gmail.com>
2019-07-25 13:45:35 -0700

    add scoring logic

--- scriptRPS.js
+++ scriptRPS.js
@@ -11,11 +11,15 @@ const paper = paperButton.innerHTML;
 const scissors = scissorsButton.innerHTML;
 const lizard = lizardButton.innerHTML;
 const spock = spockButton.innerHTML;
+const playerWin = document.getElementById('playerScore').innerHTML
 
 // initialize default game state
 let playerScore = 0;
+// console.log(playerScore);
 let computerScore = 0;
+// console.log(computerScore);
 let roundNumber = 1;
+// console.log(roundNumber);
 let playerChoice, computerChoice; // both undefined here
 
 // event listiners for all buttons on HTML
@@ -30,11 +34,24 @@ function isClicked(e) {
     getPlayerChoice(e);
     getComputerChoice(e);
     getResults();
+    // render();
 }
 
 // create playerChoice function that gets player's choice
 function getPlayerChoice(e) {
     playerChoice = e.target.innerHTML;
+}
+
+// create function to add to player score
+function addPlayerScore () {
+    playerScore ++;
+    console.log(playerScore);
+}
+
+// create function to add computer score
+function addComputerScore() {
+    computerScore ++;
+    console.log(computerScore);
 }
 
 // create computerChoice function
@@ -71,24 +88,25 @@ function getResults() {
         playerChoice === spock && computerChoice === scissors ||
         playerChoice === spock && computerChoice === rock
     ) {
-        console.log('player wins')
+        addPlayerScore();
+        console.log('Player total score ' + playerScore);
     }
     else if (
         playerChoice === computerChoice
     ) {
         console.log('its a draw')
     } else {
-        console.log('computer wins')
+        addComputerScore();
+        console.log('Computer total score ' + computerScore);
     }
 }
 
-// create round logic
-// function getRounds() {
-//     if (pla)
+// function render() {
+//     playerScore
+//     console.log(playerScore)
 // }
 
+// create scoring logic
+// create round logic
 // create game over 
-
 // create reset game logic (re-render original state after game over 
-
-
